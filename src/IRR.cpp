abad4a1e8777ec377da523b2d8bfc1f73fb70f84
parsa12309 <parsa.rashtiyan.1007@gmail.com>
2025-05-15 18:52:10 +0330

    progress: continue implementing IRR derived from Currency

--- src/IRR.cpp
+++ src/IRR.cpp
@@ -1,3 +1,11 @@
 #include "IRR.h"
 
 Irr::Irr(double& irr_currency) : Currency(irr_currency) {}
+
+double Irr::to_usd() const {
+    return get_public_currency() / 80.0;
+}
+
+std::string Irr::get_unit() const {
+    return "IRR";
+}
