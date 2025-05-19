8daf2a2eb5ecd56f191469df00ba6320da58332a
parsa12309 <parsa.rashtiyan.1007@gmail.com>
2025-05-19 23:21:29 +0330

    feat: implement operator overloads for Currency class

---
+++ src/Currency_operator.cpp
@@ -0,0 +1,69 @@
+// Currency_Operators.cpp
+#include "Currency_Operators.h"
+#include "USD.h"
+#include "EUR.h"
+#include "IRR.h"
+#include <stdexcept>
+
+std::shared_ptr<Currency> operator+(std::shared_ptr<Currency> currency1, std::shared_ptr<Currency> currency2)
+{
+    if (currency1->get_unit() != currency2->get_unit()){
+        throw std::invalid_argument("Currencies must be the same.");
+    }
+        
+    double result = currency1->get_amount() + currency2->get_amount();
+
+    if (currency1->get_unit() == "USD") {
+        return std::make_shared<Usd>(result);
+    } 
+    if (currency1->get_unit() == "EUR") {
+        return std::make_shared<Eur>(result);
+    }
+    if (currency1->get_unit() == "IRR") {
+        return std::make_shared<Irr>(result);
+    }
+
+    throw std::runtime_error("Currency type not supported.");
+}
+
+std::shared_ptr<Currency> operator-(std::shared_ptr<Currency> currency1, std::shared_ptr<Currency> currency2)
+{
+    if (currency1->get_unit() != currency2->get_unit()) {
+        throw std::invalid_argument("Currencies must be the same.");
+    }
+
+    double result = currency1->get_amount() - currency2->get_amount();
+    
+    if (currency1->get_unit() == "USD") {
+        return std::make_shared<Usd>(result);
+    }
+    if (currency1->get_unit() == "EUR") {
+        return std::make_shared<Eur>(result);
+    }
+    if (currency1->get_unit() == "IRR") {
+        return std::make_shared<Irr>(result);
+    }
+
+    throw std::runtime_error("Currency type not supported.");
+}
+
+bool operator==(std::shared_ptr<Currency> currency1, std::shared_ptr<Currency> currency2) {
+    return (currency1->get_unit() == currency2->get_unit()) && (currency1->get_amount() == currency2->get_amount());
+}
+
+bool operator!=(std::shared_ptr<Currency> currency1, std::shared_ptr<Currency> currency2) {
+    return !(currency1 == currency2);
+}
+
+bool operator<(std::shared_ptr<Currency> currency1, std::shared_ptr<Currency> currency2) {
+    return currency1->to_usd() < currency2->to_usd();
+}
+
+bool operator>(std::shared_ptr<Currency> currency1, std::shared_ptr<Currency> currency2) {
+    return currency1->to_usd() > currency2->to_usd();
+}
+
+std::ostream& operator<<(std::ostream& out, const std::shared_ptr<Currency>& currency) {
+    out << currency->get_amount() << " " << currency->get_unit();
+    return out;
+}
