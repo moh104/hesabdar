dcb10a44e83f961338ce890aba1c8b09b8c2a33b
moh104 <mg1game1m@gmail.com>
2025-05-19 22:11:48 +0330

    Add implementing constructor for item class

--- src/item.cpp
+++ src/item.cpp
@@ -9,3 +9,11 @@ ostream & operator<<(ostream& outPut , const Item& item)
 {
     return item.utilityPrint(outPut);
 }
+
+Item::Item(const string& i_name , double i_price , int i_inventory , const string &i_unit)
+{
+    setName(i_name);
+    setPrice(i_price);
+    setInventory(i_inventory);
+    setUnit(i_unit);
+}
