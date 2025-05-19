4d62ea9b18ccd24d110da9ab42b867a3d7c03ec9
parsa12309 <parsa.rashtiyan.1007@gmail.com>
2025-05-19 22:56:29 +0330

    feat: add operator prototypes to Currency class header

---
+++ include/Currency_Operators.h
@@ -0,0 +1,35 @@
+#if !defined(CURRENCY_OPERATORS)
+#define CURRENCY_OPERATORS
+
+#include <memory>
+#include <iostream>
+#include "Currency_System.h"
+
+std::shared_ptr<Currency> operator+(
+    std::shared_ptr<Currency>, std::shared_ptr<Currency>);
+
+std::shared_ptr<Currency> operator-(
+    std::shared_ptr<Currency>, std::shared_ptr<Currency>);
+
+std::shared_ptr<Currency> operator*(
+    std::shared_ptr<Currency>, std::shared_ptr<Currency>);
+
+std::shared_ptr<Currency> operator/(
+    std::shared_ptr<Currency>, std::shared_ptr<Currency>);
+bool operator==(
+    std::shared_ptr<Currency>, std::shared_ptr<Currency>);
+
+bool operator!=(
+    std::shared_ptr<Currency>, std::shared_ptr<Currency>);
+
+bool operator<(
+    std::shared_ptr<Currency>, std::shared_ptr<Currency>);
+
+bool operator>(
+    std::shared_ptr<Currency>, std::shared_ptr<Currency>);
+
+std::ostream operator<<(
+    std::ostream&, const std::shared_ptr<Currency>);
+
+
+#endif // CURRENCY_OPERATORS
