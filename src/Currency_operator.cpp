a34e2ae288c93508628f4a1320bc9cdcd7eced86
parsa12309 <parsa.rashtiyan.1007@gmail.com>
2025-05-21 10:47:42 +0330

    update:complet and refine operator overloads for Currency class

--- src/Currency_operator.cpp
+++ src/Currency_operator.cpp
@@ -11,7 +11,7 @@ std::shared_ptr<Currency> operator+(std::shared_ptr<Currency> currency1, std::sh
         throw std::invalid_argument("Currencies must be the same.");
     }
         
-    double result = currency1->get_amount() + currency2->get_amount();
+    const double result = currency1->get_amount() + currency2->get_amount();
 
     if (currency1->get_unit() == "USD") {
         return std::make_shared<Usd>(result);
@@ -31,8 +31,11 @@ std::shared_ptr<Currency> operator-(std::shared_ptr<Currency> currency1, std::sh
     if (currency1->get_unit() != currency2->get_unit()) {
         throw std::invalid_argument("Currencies must be the same.");
     }
+    if((currency1->get_amount() - currency2->get_amount()) < 0) {
+        throw std::runtime_error("The operation cannot be performed.");
+    }
 
-    double result = currency1->get_amount() - currency2->get_amount();
+    const double result = currency1->get_amount() - currency2->get_amount();
     
     if (currency1->get_unit() == "USD") {
         return std::make_shared<Usd>(result);
@@ -45,6 +48,50 @@ std::shared_ptr<Currency> operator-(std::shared_ptr<Currency> currency1, std::sh
     }
 
     throw std::runtime_error("Currency type not supported.");
+}
+
+std::shared_ptr<Currency> operator*(std::shared_ptr<Currency> currency1, std::shared_ptr<Currency> currency2)
+{
+    if(currency1->get_unit() != currency2->get_unit()) {
+        throw std::invalid_argument("Currencies must be the same.");
+    }
+
+    const auto result = currency1->get_amount() * currency2->get_amount();
+     
+    if (currency1->get_unit() == "USD") {
+        return std::make_shared<Usd>(result);
+    }
+    if (currency1->get_unit() == "EUR") {
+        return std::make_shared<Eur>(result);
+    }
+    if (currency1->get_unit() == "IRR") {
+        return std::make_shared<Irr>(result);
+    }
+
+    throw std::runtime_error("Currency type not supported.");
+
+}
+
+std::shared_ptr<Currency> operator/(std::shared_ptr<Currency> currency1, std::shared_ptr<Currency> currency2)
+{
+     if(currency1->get_unit() != currency2->get_unit()) {
+        throw std::invalid_argument("Currencies must be the same.");
+    }
+    
+    const auto result = currency1->get_amount() / currency2->get_amount();
+
+    if (currency1->get_unit() == "USD") {
+        return std::make_shared<Usd>(result);
+    }
+    if (currency1->get_unit() == "EUR") {
+        return std::make_shared<Eur>(result);
+    }
+    if (currency1->get_unit() == "IRR") {
+        return std::make_shared<Irr>(result);
+    }
+
+    throw std::runtime_error("Currency type not supported.");
+
 }
 
 bool operator==(std::shared_ptr<Currency> currency1, std::shared_ptr<Currency> currency2) {
@@ -61,9 +108,4 @@ bool operator<(std::shared_ptr<Currency> currency1, std::shared_ptr<Currency> cu
 
 bool operator>(std::shared_ptr<Currency> currency1, std::shared_ptr<Currency> currency2) {
     return currency1->to_usd() > currency2->to_usd();
-}
-
-std::ostream& operator<<(std::ostream& out, const std::shared_ptr<Currency>& currency) {
-    out << currency->get_amount() << " " << currency->get_unit();
-    return out;
 }
